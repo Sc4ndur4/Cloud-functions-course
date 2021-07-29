36aee115774ccaf865f3e05b385be08b00e26759
Sc4ndur4 <scanduraah_11@outlook.com>
2021-07-29 01:11:14 -0300

    added gogle credentials to the .gitignore

--- emails/main.py
+++ emails/main.py
@@ -1,4 +1,4 @@
-from flask import abort
+'''from flask import abort
 
 def get_bearer_token(request):
     bearer_token = request_headers.get('Authorization',None)
@@ -18,19 +18,20 @@ def get_bearer_token(request):
     return bearer_token
 
 def send_email(request):
-    '''codigo do sendgrid
+    codigo do sendgrid
     abort(405)
     bearer_token = get_bearer_token(request)
     secret_key = os.environ.get('ACCESS TOKEN')
     if bearer_token != secret_key:
     abort(401)
     request_json = request.get.json(silent=True)
-    parameters = {'sender' , 'receiver', 'subject', 'message') 
+    parameters = {'sender' , 'receiver', 'subject', 'message')
     sender, receiver, subject, message = '', '', '', ''
     if request_json and all(k in request_json for k in parameters):
         sender = request_json['sender']
         receiver = request_json['receiver']
         subject = request_json['subject']
         message = request_json['message']
-        else:
-        abort(400)''
+    else:
+        abort(400) '''
+
